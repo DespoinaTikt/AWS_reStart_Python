19ddbb7d7ac9ad6b212bf4ecaa4d27aad09a900b
DespoinaTikt <166096217+DespoinaTikt@users.noreply.github.com>
2024-04-16 10:34:26 +0300

    Add files via upload

---
+++ Categorize_values.py
@@ -0,0 +1,3 @@
+MyMixedTypeList=[45, 52158, True, 1.56, "Good morning", 84, "603"]
+for item in MyMixedTypeList:
+    print("{} is of the data type {}".format(item,type(item)))
---
+++ Lists_Tuples_Dicts.py
@@ -0,0 +1,24 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFruitList[2]='orange'
+print(myFruitList)
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary={
+    "Milo":"apple",
+    "Mpanana":"banana",
+    "Ananas":"pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Milo"])
+print(myFavoriteFruitDictionary["Mpanana"])
+print(myFavoriteFruitDictionary["Ananas"])
